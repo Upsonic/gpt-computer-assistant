d1ff7f291a557f6e985a1d2ce14e3580c60ac281
Onur ULUSOY <atadogan06@gmail.com>
2024-06-04 14:55:54 +0300

    Added try except to tiger tools

--- gpt_computer_assistant/agent/agent.py
+++ gpt_computer_assistant/agent/agent.py
@@ -11,11 +11,25 @@ from langgraph.checkpoint.sqlite import SqliteSaver
 
 from langgraph.prebuilt import chat_agent_executor
 
+try:
+    from upsonic import Tiger
+    tools = Tiger()
+    tools.enable_auto_requirements = True
+    tools = tools.langchain()
+except:
+    from langchain.agents import Tool
+    from langchain_experimental.utilities import PythonREPL
+    python_repl = PythonREPL()
+    # You can create the tool to pass to an agent
+    repl_tool = Tool(
+        name="python_repl",
+        description="A Python shell. Use this to execute python commands. Input should be a valid python command. If you want to see the output of a value, you should print it out with `print(...)`.",
+        func=python_repl.run,
+    )
+    tools = [repl_tool]
 
-from upsonic import Tiger
-tools = Tiger()
-tools.enable_auto_requirements = True
-tools = tools.langchain()
+
+
 def get_agent_executor():
     return chat_agent_executor.create_tool_calling_executor(get_model(), tools)
 
