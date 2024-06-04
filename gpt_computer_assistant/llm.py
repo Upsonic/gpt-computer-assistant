16e4d932718992559ab0759a40fdf50646a49ba5
Dawood Touseef <97373719+DawoodTouseef@users.noreply.github.com>
2024-06-04 10:23:47 +0530

    Update llm.py

--- gpt_computer_assistant/llm.py
+++ gpt_computer_assistant/llm.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 from langchain_openai import ChatOpenAI
-
+from langchain_community.chat_models import ChatOllama
 from .utils.db import load_api_key
 
 
@@ -9,6 +9,8 @@ from .utils.db import load_api_key
 
 def get_model():
     the_api_key = load_api_key()
+    if the_api_key is None:
+        return ChatOllama(model="llava")
     return ChatOpenAI(model="gpt-4o", api_key=the_api_key)
 
 
