adc47b8ce7af20ae245f8bc847f072923fe6a830
Aykut Gedik <aykutged@gmail.com>
2024-06-05 10:30:22 -0700

    add infrastack integration

---
+++ gpt_computer_assistant/utils/telemetry.py
@@ -0,0 +1,6 @@
+import os
+
+from infrastack import CreateTracer
+
+# InfraStack AI Public API Key
+my_tracer = CreateTracer("gpt_computer_assistant", "app", infrastackai_api_key="sk-2b29c6da910d2883de0599d4c5dd6b9d2e4ec61bbfa834d5")
