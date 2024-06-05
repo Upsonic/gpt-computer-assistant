8a80a8ba8b9c8e05d64e96e8b0ad040ad400d061
Onur ULUSOY <atadogan06@gmail.com>
2024-06-05 23:37:25 +0300

    Added agentic infrastructure

--- gpt_computer_assistant/__init__.py
+++ gpt_computer_assistant/__init__.py
@@ -1,5 +1,8 @@
 from .start import start
 
+from .agentic import Agent
+
+
 
 __version__ = '0.7.1'
 
--- gpt_computer_assistant/agent/assistant.py
+++ gpt_computer_assistant/agent/assistant.py
@@ -1,14 +1,105 @@
 import base64
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from crewai import Task, Crew
+
+
 from .chat_history import *
 from .agent import *
 from ..screen.shot import *
 
-from ..utils.db import load_model_settings
+from ..utils.db import load_model_settings, agents
+
+
+from ..llm import get_model
+
 
 config = {"configurable": {"thread_id": "abc123"}}
 
-def assistant(llm_input, llm_history, client, screenshot_path=None):
+
+
+
+
+
+def agentic(llm_input, llm_history, client, screenshot_path=None, dont_save_image=False):
+
+    print("LLM INPUT", llm_input)
+
+    def image_explaination():
+        the_message = [
+                    {"type": "text", "text": f"Explain the image"},
+                    
+                ]
+
+
+
+        if screenshot_path:
+            base64_image = encode_image(screenshot_path)
+            the_message.append(
+                {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{base64_image}"},
+                    },
+            )
+            print("LEN OF İMAGE", len(base64_image)) 
+
+        the_message = HumanMessage(content=the_message)
+        get_chat_message_history().add_message(the_message)
+
+
+        the_model = load_model_settings()
+
+
+        if the_model == "gpt-4o":
+            msg = get_agent_executor().invoke({"messages":llm_history + [the_message]}, config=config)
+
+        elif the_model == "llava" or the_model == "bakllava":
+
+            get_agent_executor().invoke(
+                {
+                    "input": the_message,
+                    "chat_history": llm_history,
+                }
+            )
+
+
+
+        the_last_messages = msg["messages"]
+
+        return the_last_messages[-1].content
+
+
+
+    if screenshot_path:
+        image_explain = image_explaination()
+        llm_input += "User Sent Image and image content is: " + image_explain
+
+    
+
+    task = Task(description=llm_input, expected_output="Answer", agent=agents[0])
+
+    the_crew = Crew(
+        llm=get_model(),
+        agents=agents,
+        tasks=[task],
+        full_output=True,
+        verbose=True,
+    )
+
+    result = the_crew.kickoff()["final_output"]
+
+    get_chat_message_history().add_message(HumanMessage(content=[llm_input]))
+    get_chat_message_history().add_message(AIMessage(content=[result]))
+
+    return result
+
+
+
+
+def assistant(llm_input, llm_history, client, screenshot_path=None, dont_save_image=False):
+
+    if len(agents) != 0:
+        print("Moving to Agentic")
+        return agentic(llm_input, llm_history, client, screenshot_path, dont_save_image)
 
     print("LLM INPUT", llm_input)
 
@@ -51,4 +142,27 @@ def assistant(llm_input, llm_history, client, screenshot_path=None):
 
 
 
-    return msg["messages"]
+    the_last_messages = msg["messages"]
+
+
+    if dont_save_image and screenshot_path != None:
+        currently_messages = get_chat_message_history().messages
+        if take_screenshot:
+            last_message = currently_messages[-1].content[0]
+            currently_messages.remove(currently_messages[-1])
+
+            get_chat_message_history().clear()
+            for message in currently_messages:
+                get_chat_message_history().add_message(message)
+            get_chat_message_history().add_message(HumanMessage(content=[last_message]))
+
+    get_chat_message_history().add_message(the_last_messages[-1])
+
+
+
+    return the_last_messages[-1].content
+
+
+
+
+
