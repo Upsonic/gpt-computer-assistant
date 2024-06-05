02c0b013c83134f3deefd3d1b7d057b71bdf8ba2
Onur ULUSOY <atadogan06@gmail.com>
2024-06-05 20:58:14 +0300

    Update telemetry.py

--- gpt_computer_assistant/utils/telemetry.py
+++ gpt_computer_assistant/utils/telemetry.py
@@ -1,6 +1,29 @@
-import os
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, SimpleSpanProcessor, ConsoleSpanExporter
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (
+        BatchSpanProcessor,
+        ConsoleSpanExporter,
+    )
 
-from infrastack import CreateTracer
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.trace import Status, StatusCode
 
-# InfraStack AI Public API Key
+
+def CreateTracer(service_name, trace_name, infrastackai_api_key=None):
+
+    tracer = trace.get_tracer(trace_name)
+    resource = Resource.create({"service.name": service_name})
+    provider = TracerProvider(resource=resource)
+    trace.set_tracer_provider(provider)
+
+
+    provider.add_span_processor(
+        BatchSpanProcessor(OTLPSpanExporter(endpoint="https://collector-us1-http.infrastack.ai/v1/traces", headers=(("infrastack-api-key", infrastackai_api_key),)))
+    )
+
+    return tracer
+
 my_tracer = CreateTracer("gpt_computer_assistant", "app", infrastackai_api_key="sk-2b29c6da910d2883de0599d4c5dd6b9d2e4ec61bbfa834d5")
+
