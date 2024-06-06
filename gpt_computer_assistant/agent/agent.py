57a21b680488c1be44d780fdce757aacb6ddf123
Onur ULUSOY <atadogan06@gmail.com>
2024-06-06 05:17:14 +0300

    Fixed tools

--- gpt_computer_assistant/agent/agent.py
+++ gpt_computer_assistant/agent/agent.py
@@ -13,7 +13,6 @@ from langgraph.prebuilt import chat_agent_executor
 
 try:
     from upsonic import Tiger
-    raise Exception("Use the other one")
     tools = Tiger()
     tools.enable_auto_requirements = True
     tools = tools.langchain()
