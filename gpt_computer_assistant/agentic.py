b2cc29c194c397fd5457d0c14207946aacebb9fd
Onur ULUSOY <atadogan06@gmail.com>
2024-06-06 22:22:06 +0300

    Fixed agentic infra (#61)

--- gpt_computer_assistant/agentic.py
+++ gpt_computer_assistant/agentic.py
@@ -2,10 +2,10 @@
 from .utils.db import agents
 
 class Agent():
-    def __init__(self, *args, **kwargs):
+    def __init__(self, role, goal, backstory):
         from crewai import Agent as crewai_Agent
         global agents
-        agents.append(crewai_Agent(*args, **kwargs))
+        agents.append(crewai_Agent(role=role, goal=goal, backstory=backstory))
 
 
 
