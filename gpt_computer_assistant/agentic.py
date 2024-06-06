715063af9ef3001874d0159efe8009b91a801691
Onur ULUSOY <atadogan06@gmail.com>
2024-06-06 22:24:43 +0300

    Fix

--- gpt_computer_assistant/agentic.py
+++ gpt_computer_assistant/agentic.py
@@ -1,11 +1,15 @@
 
 from .utils.db import agents
 
+
+from .llm import get_model
+
+
 class Agent():
     def __init__(self, role, goal, backstory):
         from crewai import Agent as crewai_Agent
         global agents
-        agents.append(crewai_Agent(role=role, goal=goal, backstory=backstory))
+        agents.append(crewai_Agent(role=role, goal=goal, backstory=backstory, llm=get_model()))
 
 
 
