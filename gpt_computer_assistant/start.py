84c9cc9fe8d4cfb990e235a31b8e3a2b13dcb677
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2024-06-07 17:07:06 +0530

    More Pythonic way to use is not None instead of != None

--- gpt_computer_assistant/start.py
+++ gpt_computer_assistant/start.py
@@ -1,10 +1,11 @@
 import os
-
+import argparse
+from .utils.db import set_profile
+from .gpt_computer_assistant import QApplication, MainWindow, sys
 
 def start():
 
     # get --profile argument with library
-    import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--profile", help="profile to use")
@@ -12,15 +13,9 @@ def start():
     profile = args.profile
     print("Profile:", profile)
 
-    if profile != None:
-        from .utils.db import set_profile
-
+    if profile is not None:  
         set_profile(profile)
 
-    try:
-        from .gpt_computer_assistant import QApplication, MainWindow, sys
-    except ImportError:
-        from gpt_computer_assistant import QApplication, MainWindow, sys
     os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 
     app = QApplication(sys.argv)
