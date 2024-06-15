471714ab614e0d1d490ea46eaea8b7b48e788200
Onur ULUSOY <atadogan06@gmail.com>
2024-06-15 03:23:10 +0300

    Update stt.py

--- gpt_computer_assistant/audio/stt.py
+++ gpt_computer_assistant/audio/stt.py
@@ -5,38 +5,7 @@ except ImportError:
 
 import os
 from pydub import AudioSegment
-import pvporcupine
-import pyaudio
-import struct
 
-def wake_word(access_key:str):
-    porcupine = pvporcupine.create(access_key=access_key,
-                                           keywords=pvporcupine.KEYWORDS)
-            # Initialize PyAudio
-            pa = pyaudio.PyAudio()
-
-            # Open an audio stream
-            audio_stream = pa.open(
-                rate=porcupine.sample_rate,
-                channels=1,
-                format=pyaudio.paInt16,
-                input=True,
-                frames_per_buffer=porcupine.frame_length
-            )
-
-            print_("Listening for wake word...",color="yellow")
-
-            # Continuously listen for the wake word
-            while True:
-                pcm = audio_stream.read(porcupine.frame_length)
-                pcm = struct.unpack_from("h" * porcupine.frame_length, pcm)
-
-                # Process the audio frame and check for the wake word
-                keyword_index = porcupine.process(pcm)
-
-                if keyword_index >= 0:
-                    print_("Wake word detected!",color="green")
-                    return True
 
 def split_audio(file_path, max_size=20 * 1024 * 1024):
     """Split an audio file into smaller parts if it exceeds a maximum size.
