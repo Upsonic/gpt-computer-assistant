9ab488328a4fcade9666482cc0cfb1a5bcd515c8
Onur ULUSOY <atadogan06@gmail.com>
2024-07-30 13:00:21 +0300

    feat: Aded .dmg file generation script for macos

---
+++ macos_build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Pyinstaller
+pyinstaller --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.icns" --name="GPT Computer Assistant"
+# Create a DMG
+# Create a folder (named dmg) to prepare our DMG in (if it doesn't already exist).
+mkdir -p dist/dmg
+# Empty the dmg folder.
+rm -r dist/dmg/*
+# Copy the app bundle to the dmg folder.
+cp -r "dist/GPT Computer Assistant.app" dist/dmg
+# If the DMG already exists, delete it.
+test -f "dist/GPT Computer Assistant.dmg" && rm "dist/GPT Computer Assistant.dmg"
+create-dmg \
+  --volname "GPT Computer Assistant" \
+  --volicon "gpt_computer_assistant/utils/media/icon.icns" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "GPT Computer Assistant.app" 175 120 \
+  --hide-extension "GPT Computer Assistant.app" \
+  --app-drop-link 425 120 \
+  "dist/GPT Computer Assistant.dmg" \
+  "dist/dmg/"
