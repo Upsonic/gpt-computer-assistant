803fb17c1fb958e2221e5d3ed5b9437c9792cfd2
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 12:31:59 +0300

    chore: Update Python version to 3.11 in build workflow

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -6,5 +6,5 @@ python -m pip install pyinstaller==6.9.0
 
 
 # Pyinstaller
-pyinstaller --hidden-import PyQt5  --recursive-copy-metadata gpt_computer_assistant run.py --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant" --onefile
+pyinstaller  --windowed  --recursive-copy-metadata gpt_computer_assistant run.py --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant" --onefile
 
