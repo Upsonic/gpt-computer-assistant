fb010318fadbd9683b31771f76a6bdaa9426a8a1
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 21:45:08 +0300

    chore: Update Windows build script to use latest dependencies and PyInstaller configuration

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
+# create an function to check system is windows
+def is_windows():
+    import platform
+    return platform.system() == 'Windows'
+
+# check if system is windows
+if is_windows():
+    # if system is windows, then start the application
+    # fix start
+    import sys
+    import os
+
+    if sys.stdout is None:
+        sys.stdout = open(os.devnull, "w")
+    if sys.stderr is None:
+        sys.stderr = open(os.devnull, "w")
+    # fix end
+
+
+
 from gpt_computer_assistant import start
+
+
+
 
 
 
