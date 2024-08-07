68b501ccd37161e2789ad56a307654d62eb0870d
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 17:54:21 +0300

    chore: Update Windows build script to use latest dependencies and PyInstaller configuration

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-# Requireed installations
-python -m pip install ".[base]"
-python -m pip install '.[agentic]'
-python -m pip install pyinstaller==6.9.0
 
 
 # Pyinstaller
-pip3 uninstall pyqt5 -y
-pip3 install pyqt5==5.15.10
-pip3 uninstall pynput -y
-pip3 install pynput==1.7.3
+pip3 install '.[base]'
+pip3 install '.[agentic]'
 pyinstaller --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant"
