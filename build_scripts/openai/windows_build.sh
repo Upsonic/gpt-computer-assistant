458ef5a986d76c6dd7d69e231f97ae88fc772c97
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 12:41:58 +0300

    chore: Update Windows build script to use app.spec for PyInstaller configuration

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -6,5 +6,4 @@ python -m pip install pyinstaller==6.9.0
 
 
 # Pyinstaller
-pyinstaller  --windowed  --recursive-copy-metadata gpt_computer_assistant run.py --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant" --onefile
-
+pyinstaller app.spec
