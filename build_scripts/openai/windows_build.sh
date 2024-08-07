728d1ddbbb3ad380099674ee85c18f789cf8773d
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 17:59:16 +0300

    chore: Update Windows build script to use pyinstaller==6.9.0

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -4,4 +4,5 @@
 # Pyinstaller
 pip3 install '.[base]'
 pip3 install '.[agentic]'
+pip3 install pyinstaller==6.9.0
 pyinstaller --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant"
