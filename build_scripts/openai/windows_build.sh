5f6a8d25e223a31635b61bc8492276a6d6f8d59f
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 16:26:13 +0300

    chore: Update Windows build script to use hidden-import for pynput library in PyInstaller configuration

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -8,4 +8,4 @@ python -m pip install pyinstaller==6.9.0
 # Pyinstaller
 pip3 uninstall pyqt5 -y
 pip3 install pyqt5==5.15.10
-pyinstaller --additional-hooks-dir="build_scripts/hooks" --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant"
+pyinstaller --hidden-import pynput --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant"
