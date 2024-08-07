289bbff89d21bd20c9a14a975f425e6cdc8e29ed
Onur ULUSOY <atadogan06@gmail.com>
2024-08-07 14:51:47 +0300

    chore: Update .gitignore to exclude *.spec files

--- build_scripts/openai/windows_build.sh
+++ build_scripts/openai/windows_build.sh
@@ -6,4 +6,4 @@ python -m pip install pyinstaller==6.9.0
 
 
 # Pyinstaller
-pyinstaller app.spec
+pyinstaller --hidden-import PyQt5.sip --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.ico" --name="GPT Computer Assistant"
