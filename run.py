3641a8d2dcad5e5ef77b615e74f7097d28e64c29
Onur ULUSOY <atadogan06@gmail.com>
2024-08-08 09:03:46 +0300

    chore: Update Windows build script to use latest numpy version

--- run.py
+++ run.py
@@ -1,22 +1,15 @@
-# create an function to check system is windows
-def is_windows():
-    import platform
-    return platform.system() == 'Windows'
-
-# check if system is windows
-if is_windows():
-    # if system is windows, then start the application
-    # fix start
-    import sys
-    import os
-
-    if sys.stdout is None:
-        sys.stdout = open(os.devnull, "w")
-    if sys.stderr is None:
-        sys.stderr = open(os.devnull, "w")
-    # fix end
+import numpy
 
 
+# fix start
+import sys
+import os
+
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w")
+    
 
 from gpt_computer_assistant import start
 
