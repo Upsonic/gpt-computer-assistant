acbef88126d1dbc853acf9d65938b6e2fa9b7caa
Onur ULUSOY <atadogan06@gmail.com>
2024-08-11 00:05:50 +0300

    Refactor build scripts for macOS and Windows

--- build_scripts/openai/macos_build.sh
+++ build_scripts/openai/macos_build.sh
@@ -7,24 +7,24 @@ pip install pyinstaller==6.9.0
 
 
 # Pyinstaller
-pyinstaller --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.icns" --name="GPT Computer Assistant"
+pyinstaller --recursive-copy-metadata gpt_computer_assistant run.py --windowed --add-data="gpt_computer_assistant/utils/media/*":"gpt_computer_assistant/utils/media" --icon="gpt_computer_assistant/utils/media/icon.icns" --name="GPT_Computer_Assistant"
 # Create a DMG
 # Create a folder (named dmg) to prepare our DMG in (if it doesn't already exist).
 mkdir -p dist/dmg
 # Empty the dmg folder.
 rm -r dist/dmg/*
 # Copy the app bundle to the dmg folder.
-cp -r "dist/GPT Computer Assistant.app" dist/dmg
+cp -r "dist/GPT_Computer_Assistant.app" dist/dmg
 # If the DMG already exists, delete it.
-test -f "dist/GPT Computer Assistant.dmg" && rm "dist/GPT Computer Assistant.dmg"
+test -f "dist/GPT_Computer_Assistant.dmg" && rm "dist/GPT_Computer_Assistant.dmg"
 create-dmg \
-  --volname "GPT Computer Assistant" \
+  --volname "GPT_Computer_Assistant" \
   --volicon "gpt_computer_assistant/utils/media/icon.icns" \
   --window-pos 200 120 \
   --window-size 600 300 \
   --icon-size 100 \
-  --icon "GPT Computer Assistant.app" 175 120 \
-  --hide-extension "GPT Computer Assistant.app" \
+  --icon "GPT_Computer_Assistant.app" 175 120 \
+  --hide-extension "GPT_Computer_Assistant.app" \
   --app-drop-link 425 120 \
-  "dist/GPT Computer Assistant.dmg" \
+  "dist/GPT_Computer_Assistant.dmg" \
   "dist/dmg/"
