a9dcee5bdbe247e3d9bddd2e2129f684b17bb734
Onur ULUSOY <atadogan06@gmail.com>
2025-01-07 14:51:20 +0300

    feat: Introduce ToolList response format in test_call.py and enhance tool validation in tests. This update improves the structure of tool responses and ensures accurate assertions for tool presence in the results.

--- tests/client/test_call.py
+++ tests/client/test_call.py
@@ -17,6 +17,9 @@ class City(BaseModel):
     population: int
 
 
+class ToolList(BaseModel):
+    tools: list[str]
+
 def test_gpt4o_call():
     result = server.call(prompt="Hi, I am Onur Atakan ULUSOY and I am a male", response_format=Human)
     print(result)
@@ -46,12 +49,36 @@ def test_call_with_tools():
 
 
 def test_call_with_mcp_server():
-    result = server.call(prompt="What are your tools?", tools=["add_numbers", "fetch"], mcp_servers=[{"command": "uvx", "args": ["mcp-server-fetch"]}])
+    result = server.call(prompt="What are your tools?", response_format=ToolList, tools=["add_numbers", "fetch"], mcp_servers=[{"command": "uvx", "args": ["mcp-server-fetch"]}])
     print(result)
-    assert "add_numbers" in result and "fetch" in result
+
+    any_found_add_numbers = False
+    any_found_fetch = False
+    for tool in result.tools:
+        if "add_numbers" in tool.lower():
+            any_found_add_numbers = True
+            
+        if "fetch" in tool.lower():
+            any_found_fetch = True
+            
+
+    print(any_found_add_numbers, any_found_fetch)
+    assert any_found_add_numbers and any_found_fetch
 
 
 def test_call_with_env_variable():
-    result = server.call(prompt="What are your tools?", tools=["add_numbers", "fetch"], mcp_servers=[{"command": "uvx", "args": ["mcp-server-fetch"], "env": {"test": "test"}}],)
+    result = server.call(prompt="What are your tools?", response_format=ToolList, tools=["add_numbers", "fetch"], mcp_servers=[{"command": "uvx", "args": ["mcp-server-fetch"], "env": {"test": "test"}}],)
     print(result)
-    assert "add_numbers" in result and "fetch" in result
+
+    any_found_add_numbers = False
+    any_found_fetch = False
+    for tool in result.tools:
+        if "add_numbers" in tool.lower():
+            any_found_add_numbers = True
+            
+        if "fetch" in tool.lower():
+            any_found_fetch = True
+            
+
+    print(any_found_add_numbers, any_found_fetch)
+    assert any_found_add_numbers and any_found_fetch
