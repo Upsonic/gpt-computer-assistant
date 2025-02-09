ed5462833c6b7f5478280922590d444881be3473
Onur ULUSOY <atadogan06@gmail.com>
2025-02-10 01:36:43 +0300

    feat: Initialize Upsonic tools module with placeholder classes

---
+++ src/upsonic/tools.py
@@ -0,0 +1,17 @@
+"""
+Upsonic Tools Module
+This module contains the tool implementations that can be used with Upsonic.
+"""
+from typing import Any, List, Dict, Optional, Type, Union, Callable
+
+class Search:
+    pass
+
+class ComputerUse:
+    pass
+
+class BrowserUse:
+    pass
+
+# Export all tool classes
+__all__ = ["Search", "ComputerUse", "BrowserUse"] 
